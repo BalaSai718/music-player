cc33e4910fdb2c34b37ced352447b6823e9e8605
Bala Sai <90441731+BalaSai718@users.noreply.github.com>
2021-12-01 19:11:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,124 +71,133 @@ let Songs = [
   },
 ];
 
-let previous = document.querySelector('#prev');
-let play = document.querySelector('#play');
-let next = document.querySelector('#next');
-let song_name = document.querySelector('#name');
-let volume = document.querySelector('#sound');
-let sound_value = document.querySelector('#sound_value');
-let song_thumbnail = document.querySelector('#song_thumbnail');
-let slider = document.querySelector('#duration_slider');
-let song_duration = document.querySelector('#song_duration');
-let auto_play = document.querySelector('#auto_play');
-let current = document.querySelector('#current');
-let total = document.querySelector('#total');
-let singer = document.querySelector('#singer');
+let previous = document.querySelector("#prev");
+let play = document.querySelector("#play");
+let next = document.querySelector("#next");
+let title = document.querySelector("#title");
+let recent_volume = document.querySelector("#volume");
+let volume_show = document.querySelector("#volume_show");
+let slider = document.querySelector("#duration_slider");
+let show_duration = document.querySelector("#show_duration");
+let song_img = document.querySelector("#song_img");
+let auto_play = document.querySelector("#auto");
+let curr_song = document.querySelector("#curr_song");
+let total = document.querySelector("#total");
+let artist = document.querySelector("#artist");
 
 let timer;
 let autoplay = 0;
-let index_num = 0;
-let current_song = false;
-let track = document.createElement('audio')
 
+let index_no = 0;
+let Playing_song = false;
 
-function load_track(index_num){
-    clearInterval(timer);
-    track.src = Songs[index_num].path;
-    song_name.innerHTML = Songs[index_num].name;
-    song_thumbnail.src = Songs[index_num].image;
-    singer.innerHTML = Songs[index_num].artist;
-    track.load();
+let track = document.createElement("audio");
 
-    total.innerHTML = Songs.length;
-    current.innerHTML = index_num + 1;
+function load_track(index_no) {
+  clearInterval(timer);
+  reset_slider();
+
+  track.src = Songs[index_no].path;
+  title.innerHTML = Songs[index_no].name;
+  song_img.src = Songs[index_no].image;
+  artist.innerHTML = Songs[index_no].artist;
+  track.load();
+
+  timer = setInterval(range_slider, 1000);
+  total.innerHTML = Songs.length;
+  curr_song.innerHTML = index_no + 1;
 }
 
-load_track(index_num);
+load_track(index_no);
 
-function mute_sound(){
-    track.sound = 0;
-    sound.value = 0;
-    sound_value.innerHTML = 0;
+function mute_sound() {
+  track.volume = 0;
+  volume.value = 0;
+  volume_show.innerHTML = 0;
 }
 
-function justplay(){
-    if(current_song==false){
-        playsong();
-    }else{
-        pausesong();
-    }
+function justplay() {
+  if (Playing_song == false) {
+    playsong();
+  } else {
+    pausesong();
+  }
 }
 
-function playsong(){
-    track.play();
-    current_song = true;
-    play.innerHTML = '<i class="fa fa-pause"></i>';
+function reset_slider() {
+  slider.value = 0;
 }
 
-function pausesong(){
-    track.pause();
-    current_song = false;
-    play.innerHTML = '<i class="fa fa-play"></i>';
+function playsong() {
+  track.play();
+  Playing_song = true;
+  play.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
 }
 
-function next_song(){
-    if (index_num < Songs.length - 1){
-        index_num += 1;
-        load_track(index_num);
-        playsong();
-    }else{
-        index_num = 0;
-        load_track(index_num);
-        playsong();
-    }
+function pausesong() {
+  track.pause();
+  Playing_song = false;
+  play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
 }
 
-function previous_song(){
-    if (index_num > 0){
-        index_num -= 1;
-        load_track(index_num);
-        playsong();
-    }else{
-        index_num = Songs.length;
-        load_track(index_num);
-        playsong();
-    }
+function next_song() {
+  if (index_no < Songs.length - 1) {
+    index_no += 1;
+    load_track(index_no);
+    playsong();
+  } else {
+    index_no = 0;
+    load_track(index_no);
+    playsong();
+  }
 }
 
-function volume_change(){
-    sound_value.innerHTML = volume.value;
-    track.sound = volume.value / 100;
+function previous_song() {
+  if (index_no > 0) {
+    index_no -= 1;
+    load_track(index_no);
+    playsong();
+  } else {
+    index_no = Songs.length;
+    load_track(index_no);
+    playsong();
+  }
 }
 
-function change_duration(){
+function volume_change() {
+  volume_show.innerHTML = recent_volume.value;
+  track.volume = recent_volume.value / 100;
+}
+
+function change_duration() {
   slider_position = track.duration * (slider.value / 100);
   track.currentTime = slider_position;
 }
 
-function autoplay_switch(){
-  if (autoplay==1){
-      autoplay=0;
-      auto_play.style.background = "rgba(255,255,255,0.2)";
-  }else{
-      autoplay = 1;
-      auto_play.style.background ="#FF8A65";
+function autoplay_switch() {
+  if (autoplay == 1) {
+    autoplay = 0;
+    auto_play.style.background = "rgba(255,255,255,0.2)";
+  } else {
+    autoplay = 1;
+    auto_play.style.background = "#148F77";
   }
 }
 
-function range_slider(){
+function range_slider() {
   let position = 0;
-  if(!isNaN(track.duration)){
-      position = track.currentTime * (100/ track.duration);
-      slider.value = position;
+
+  if (!isNaN(track.duration)) {
+    position = track.currentTime * (100 / track.duration);
+    slider.value = position;
   }
 
-  if(track.ended){
-      play.innerHTML = '<i class="fa fa-play"></i>';
-      if (autoplay==1){
-          index_num += 1;
-          load_track(index_num);
-          playsong();
-      }
+  if (track.ended) {
+    play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+    if (autoplay == 1) {
+      index_no += 1;
+      load_track(index_no);
+      playsong();
+    }
   }
 }
